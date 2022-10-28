014e52b3699c3bbea7050e25e5d1cb22171d4d3d
MrBlink <slajwith@gmail.com>
2022-10-28 09:52:33 +0530

    QR Code React Repository Initialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import React from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>This is QR Code</div>
+  )
+}
+
+export default App
