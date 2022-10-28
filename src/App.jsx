7f7e2388e3f5821d920a08cef62a0b8ce5574d15
MrBlink <slajwith@gmail.com>
2022-10-28 10:52:18 +0530

    Frontend Mentor QR Code Challenge Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import React from 'react';
+import React from "react";
+import qrCode from "./assets/images/image-qr-code.png";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App= () => {
 
   return (
-    <div>This is QR Code</div>
-  )
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col max-w-qr text-center p-6 m-4 bg-white rounded-2xl font-outfit">
+        <img src={qrCode} alt="Main QR Code Image" className="rounded-2xl"/>
+        <div className="p-6">
+          <h2 className="font-bold text-xl text-darkBlue mb-4">Improve your Front-End Skills By Building Projects</h2>
+          <p className="text-grayishBlue text-md">
+            Scan the QR Code to Visit Frontend Mentor and Take your coding
+            skills to next level.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,18 @@ module.exports = {
     extend: {
       fontFamily:{
         'outfit' : ['Outfit', 'sans-serif'],
+      },
+      colors:{
+        'lightGray':'hsl(212, 45%, 89%)',
+        'grayishBlue':'hsl(220, 15%, 55%)',
+        'darkBlue':'hsl(218, 44%, 22%)'
       }
+
     },
+
+    maxWidth:{
+      'qr':'360px'
+    }
   },
   plugins: [],
 }
